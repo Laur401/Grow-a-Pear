2fd8b1d4250d23b4dd824286568213d5f4a0a316
Your Name <183149493+Laur401@users.noreply.github.com>
2024-10-02 17:37:48 +0200

    FINALLY fixed the gate collision. I wish I hadn't spent 5 hours on this. EOS

--- Assets/Scripts/ExtendableObject.cs
+++ Assets/Scripts/ExtendableObject.cs
@@ -6,8 +6,11 @@ public class ExtendableObject : MonoBehaviour, IActivator
 {
     [SerializeField] private Transform stopPoint;
     [SerializeField] private float extendSpeed=1.0f;
-
+    //[SerializeField] GameObject spriteMaskGameObject;
+    
     private Vector3 initialPosition;
+    private BoxCollider2D objectCollider;
+    private SpriteMask spriteMask;
     private bool active=false;
     public bool Active
     {
@@ -18,6 +21,8 @@ public class ExtendableObject : MonoBehaviour, IActivator
     void Start()
     {
         initialPosition=transform.position;
+        objectCollider=GetComponent<BoxCollider2D>();
+        //spriteMask=spriteMaskGameObject.GetComponent<SpriteMask>();
     }
     
     void Update()
@@ -35,6 +40,7 @@ public class ExtendableObject : MonoBehaviour, IActivator
         {
             direction.Normalize();
             transform.position+=direction * (extendSpeed * Time.deltaTime);
+            //ColliderEdit();
         }
     }
 
@@ -47,4 +53,20 @@ public class ExtendableObject : MonoBehaviour, IActivator
             transform.position+=direction*(extendSpeed*Time.deltaTime);
         }
     }
+
+    /*private void ColliderEdit()
+    {
+        var spriteMaskBounds = spriteMask.sprite.bounds;
+        
+        if (spriteMaskBounds.Contains(objectCollider.bounds.min)&&spriteMaskBounds.Contains(objectCollider.bounds.max))
+            objectCollider.enabled=false;
+        else if (transform.position.x < spriteMaskBounds.max.x && transform.position.x > spriteMaskBounds.min.x)
+        {
+            objectCollider.enabled=true;
+            float difference = objectCollider.size.x - transform.position.x;
+            objectCollider.size = new Vector2(difference, objectCollider.size.y);
+            objectCollider.offset = new Vector2(objectCollider.offset.x + difference / 2, objectCollider.offset.y);
+        }
+        
+    }*/
 }
